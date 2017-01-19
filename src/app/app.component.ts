da9c8c211304e682a583d7baa782dda91a7985ca
Joshua Philips <jmichaelphilips@gmail.com>
2017-01-19 17:00:28 -0600

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+<app-simple-form></app-simple-form> 
+  `
+})
+export class AppComponent {
+  title = 'Hello world!';
+}
